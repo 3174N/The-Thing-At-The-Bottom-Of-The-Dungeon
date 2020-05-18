c0dcba0a3f88f5649d816b0ff714c6f2512337ac
3174N <63750650+3174N@users.noreply.github.com>
2020-05-18 12:49:03 +0300

    Added coins

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    #region variables 
+    public float amount;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        playerMovement player = other.GetComponent<playerMovement>();
+        if (player != null)
+        {
+            Apply(player);
+
+            Destroy(gameObject);
+        }
+    }
+
+    public void Apply(playerMovement player)
+    {
+        player.gameObject.GetComponent<Player>().ChangeCoins((int)amount);
+    }
+}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    #region variables 
+    int coins;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeCoins(int amount)
+    {
+        coins += amount;
+    }
+}
