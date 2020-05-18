1a675fe8a0b1c62dbc04206ebd3e1237a09b4ac4
3174N <63750650+3174N@users.noreply.github.com>
2020-05-18 17:04:17 +0300

    Added shop gameObject

---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    #region variables 
+    public Material defaultM, highlight;
+
+    SpriteRenderer sprite;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        sprite.material = highlight;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        sprite.material = defaultM;
+    }
+
+    public void Enter()
+    {
+
+    }
+}
