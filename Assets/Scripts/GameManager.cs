35b1750192866882c61ec169c6447c83874bc2b3
Embersands game Studios <eitan.levy.159@gmail.com>
2020-07-05 09:43:42 +0300

    Added gems

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("SHOP")]
+    public int gems;
+    public Text gemsText;
+
+    [Header("STARTING BONUSES")]
     public int startingCoins;
     public int startingSpeed;
     public int maxHealth = 300;
@@ -13,5 +18,10 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
         DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Update()
+    {
+        gemsText.text = gems.ToString();
     }
 }
--- Assets/Scripts/Upgrade.cs
+++ Assets/Scripts/Upgrade.cs
@@ -8,21 +8,25 @@ public class Upgrade : MonoBehaviour
     public bool speed, health, damage, coins;
     public int amount;
 
+    public int price;
+
     public int uses;
     int used;
 
     public Text bonusText;
     public Text usesText;
+    public Text priceText;
 
     private void Start()
     {
         bonusText.text = "+" + amount;
         usesText.text = used + " / " + uses;
+        priceText.text = price.ToString();
     }
 
     public void Buy()
     {
-        if (used >= uses)
+        if (used >= uses || price > Finder.GetGameManager().gems)
             return;
 
         used += 1;
@@ -45,5 +49,9 @@ public class Upgrade : MonoBehaviour
         }
 
         usesText.text = used + " / " + uses;
+
+        Finder.GetGameManager().gems -= price;
+        price *= 2;
+        priceText.text = price.ToString();
     }
 }
