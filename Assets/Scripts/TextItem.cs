cb9abe390578ad77ae5ee537dc77bd19962d0aad
Embersands game Studios <eitan.levy.159@gmail.com>
2020-07-19 11:16:24 +0300

    Added tutorial item

---
+++ Assets/Scripts/TextItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class TextItem
+{
+    public string text;
+
+    public Color color = Color.white;
+
+    public float duration;
+}
---
+++ Assets/Scripts/TutorialItem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialItem : MonoBehaviour
+{
+    public TextItem[] texts;
+
+    public GameObject canvas;
+    public Text textUI;
+
+    bool hasShown;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (hasShown)
+            return;
+
+        StartCoroutine(Display());
+
+        hasShown = true;
+    }
+
+    IEnumerator Display()
+    {
+        foreach (TextItem text in texts)
+        {
+            canvas.SetActive(true);
+
+            textUI.text = text.text;
+            textUI.color = text.color;
+
+            yield return new WaitForSeconds(text.duration);
+        }
+
+        canvas.SetActive(false);
+    }
+}
